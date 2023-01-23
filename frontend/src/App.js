c4bfa22ae7f906eb814adffc41f4f1f4a99ea2dc
Colin Francis <francico@oregonstate.edu>
2023-01-23 09:37:13 -0500

    configured react app for redux and created initial redux store

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,20 @@ import {
   BrowserRouter as Router, 
   Routes, 
   Route, 
-  Navigate } from 'react-router-dom';
+  Navigate 
+} from 'react-router-dom';
 import LoginSignup from './pages/LoginSignup';
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Navigate to='/login-signup'/>}/>
-        <Route path='/login-signup' element={<LoginSignup/>}/>
-      </Routes>
-    </Router>
+    <div id=''>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Navigate to='/login-signup'/>}/>
+          <Route path='/login-signup' element={<LoginSignup/>}/>
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
