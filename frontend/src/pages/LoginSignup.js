b81684fd148bf12a97c3b9990cc8e70a1fe5b517
Colin Francis <francico@oregonstate.edu>
2023-01-24 19:05:15 -0500

    added form separator and signup/login with google button to signup and login form

--- frontend/src/pages/LoginSignup.js
+++ frontend/src/pages/LoginSignup.js
@@ -18,12 +18,12 @@ const LoginSignup = () => {
                                     <p className='text-muted text-center'>Signup or login to start tracking jobs</p>
                                 </div>
                                 <div>
-                                    <ul className="nav nav-tabs">
-                                        <li className="nav-item">
-                                            <a className="nav-link active" data-bs-toggle='tab' href="#signup">Signup</a>
+                                    <ul className='nav nav-tabs'>
+                                        <li className='nav-item'>
+                                            <a className="nav-link active" data-bs-toggle='tab' href='#signup'>Signup</a>
                                         </li>
-                                        <li className="nav-item">
-                                            <a className="nav-link" data-bs-toggle='tab' href="#login">Login</a>
+                                        <li className='nav-item'>
+                                            <a className='nav-link' data-bs-toggle='tab' href='#login'>Login</a>
                                         </li>
                                     </ul>
                                 </div>
@@ -31,22 +31,22 @@ const LoginSignup = () => {
                                     <div className='tab-pane active mt-3' id='signup' role='tabpanel'>
                                         <div className='row'>
                                             <div className='col'>
-                                                <div class="mb-2">
-                                                    <label for="first-name" class="form-label">First name</label>
-                                                    <input type="text" class="form-control" id="first-name"/>
+                                                <div class='mb-2'>
+                                                    <label for='first-name' class='form-label'>First name</label>
+                                                    <input type='text' class='form-control' id='first-name'/>
                                                 </div>
                                             </div>
                                             <div className='col'>
-                                                <div class="mb-2">
-                                                    <label for="last-name" class="form-label">Last name</label>
-                                                    <input type="text" class="form-control" id="last-name"/>
+                                                <div class='mb-2'>
+                                                    <label for='last-name' class='form-label'>Last name</label>
+                                                    <input type='text' class='form-control' id='last-name'/>
                                                 </div>
                                             </div>
                                         </div>
                                         <div className='row'>
-                                            <div class="mb-2">
-                                                <label for="email" class="form-label">Email</label>
-                                                <input type="email" class="form-control" id="email"/>
+                                            <div class='mb-2'>
+                                                <label for='email' class='form-label'>Email</label>
+                                                <input type='email' class='form-control' id='email'/>
                                             </div>
                                         </div>
                                         <div className='row'>
@@ -56,12 +56,19 @@ const LoginSignup = () => {
                                             </div>
                                         </div>
                                         <div className='row'>
-                                            <div class="mb-2">
+                                            <div className="mb-2">
                                                 <label for="confirm-password" class="form-label">Confirm password</label>
                                                 <input type="password" class="form-control" id="password"/>
                                             </div>
                                         </div>
                                         <button type="submit" class="btn btn-primary mt-2">Signup</button>
+                                        <div className='form-separator mt-3'>or</div>
+                                        <div className="d-grid mt-3">
+                                            <button className="btn btn-primary google-button-container" type="button">
+                                                <i className="fa-brands fa-google"></i>
+                                                <span>Signup with Google</span>
+                                            </button>
+                                        </div>
                                     </div>
                                     <div className='tab-pane mt-3' id='login' role='tabpanel'>
                                         <div className='row'>
@@ -77,6 +84,13 @@ const LoginSignup = () => {
                                             </div>
                                         </div>
                                         <button type="submit" class="btn btn-primary mt-2">Login</button>
+                                        <div className='form-separator mt-3'>or</div>
+                                        <div class="d-grid mt-3">
+                                            <button className="btn btn-primary google-button-container" type="button">
+                                                <i className="fa-brands fa-google"></i>
+                                                <span>Login with Google</span>
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
                             </form>
