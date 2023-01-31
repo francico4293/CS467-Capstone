f7bd684e0880322720394838874610a1577cbecd
Ron Riza <ronriza91@gmail.com>
2023-01-31 18:22:40 -0500

    added endpoints and fucntions for create, update, delete users

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,5 +1,5 @@
 const usersRouter = require('express').Router()
-const { createUser } = require('../models/user')
+const { createUser, getUser, editUser, deleteUser } = require('../models/user')
 
 usersRouter.post('/', async (req, res) => {
     if (!req.currentUser) {
@@ -11,5 +11,30 @@ usersRouter.post('/', async (req, res) => {
     res.status(201).json(user);
 })
 
+usersRouter.get('/', async (req, res) => {
+    if (!req.currentUser) {
+        return res.status(401).json({ error: 'token missing or invalid' })
+    }
+    const { uid, email } = req.currentUser
+    const user = await getUser(uid, email)
+    res.status(200).json(user)
+})
+
+usersRouter.put('/', async (req, res) => {
+    if (!req.currentUser) {
+        return res.status(401).json({ error: 'token missing or invalid' })
+    }
+    const { uid } = req.currentUser
+    const user = await editUser(uid, req.body)
+    res.status(200).json(user)
+})
+
+usersRouter.delete('/', async (req, res) => {
+    if (!req.currentUser) {
+        return res.status(401).json({ error: 'token missing or invalid' })
+    }
+    await deleteUser(uid)
+    res.status(204).send()
+})
 
 module.exports = usersRouter
--- backend/models/user.js
+++ backend/models/user.js
@@ -12,4 +12,26 @@ createUser = async (uid, firstName, lastName, email) => {
     return {...doc.data(), id: doc.id}
 }
 
-module.exports = { createUser }
+getUser = async (uid, email) => {
+    const docRef = db.collection('users').doc(uid);
+    const doc = await docRef.get()
+    if (!doc.exists) {
+        return createUser(uid, '', '', email)
+    } else{
+        return {...doc.data(), id: doc.id}
+    }
+}
+
+editUser = async (uid, props) => {
+    const docRef = db.collection('users').doc(uid);
+    await docRef.update(props)
+
+    const doc = await docRef.get()
+    return {...doc.data(), id: doc.id}
+}
+
+deleteUser = async (uid) => {
+    await db.collection('users').doc(uid).delete()
+}
+
+module.exports = { createUser, getUser, editUser, deleteUser }
