c20f092c4b9c67c75039aa3973d665962c1159d1
Ron Riza <ronriza91@gmail.com>
2023-01-30 23:15:41 -0500

    set up express server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,31 @@
 require('dotenv').config();
+require('express-async-errors')
+const express = require('express')
+const path = require('path')
+const middleware = require('./middleware')
+
+const PORT = process.env.PORT || 3000;
+const app = express()
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(middleware.decodeIDToken)
+
+
+// send static files if no route matches
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+    console.log(err)
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Something went wrong'
+    res.status(statusCode).json({ error: err.message })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
