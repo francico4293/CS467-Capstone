23a21d9b3c49fbd833432899c6e1ed8e2c8c33e3
Colin Francis <francico@oregonstate.edu>
2023-01-31 23:09:11 -0500

    implemented onClose handler

--- frontend/src/components/EditProfileModal.js
+++ frontend/src/components/EditProfileModal.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import { editUser } from '../services/users';
 
 const EditProfileModal = ({ show, setShow }) => {
     const user = useSelector(state => state.user);
     const theme = useSelector(state => state.theme);
 
+    const [picture, setPicture] = useState(user.data.picture);
+    const [firstName, setFirstName] = useState(user.data.firstName);
+    const [lastName, setLastName] = useState(user.data.lastName);
+    const [email, setEmail] = useState(user.data.email);
+    const [description, setDescription] = useState(user.data.description);
+
+    const handleSave = () => {
+
+    }
+
+    const handleClose = () => {
+        setFirstName(user.data.firstName);
+        setLastName(user.data.lastName);
+        setEmail(user.data.email);
+        setDescription(user.data.description);
+        setShow(false);
+    }
+
     return (
-        <Modal id={`${theme}`} className='edit-proficiency-modal' show={show} onHide={() => setShow(false)} centered>
+        <Modal id={`${theme}`} className='edit-proficiency-modal' show={show} onHide={handleClose} centered>
             <Modal.Header closeButton>
                 <Modal.Title>Edit Profile</Modal.Title>
             </Modal.Header>
@@ -23,32 +42,32 @@ const EditProfileModal = ({ show, setShow }) => {
                     <div className='row mb-2'>
                         <div className='col-6'>
                             <label for='first-name' className='form-label'>First name</label>
-                            <input type='text' className='form-control' id='first-name' value={user.data.firstName}/>
+                            <input type='text' className='form-control' id='first-name' value={firstName} onChange={e => setFirstName(e.target.value)}/>
                         </div>
                         <div className='col-6'>
                             <label for='last-name' className='form-label'>Last name</label>
-                            <input type='text' className='form-control' id='last-name' value={user.data.lastName}/>
+                            <input type='text' className='form-control' id='last-name' value={lastName} onChange={e => setLastName(e.target.value)}/>
                         </div>
                     </div>
                     <div className='row mb-2'>
                         <div className='col-12'>
                             <label for='email' className='form-label'>Email</label>
-                            <input type='email' className='form-control' id='email' value={user.data.email}/>
+                            <input type='email' className='form-control' id='email' value={email} onChange={e => setEmail(e.target.value)}/>
                         </div>
                     </div>
                     <div className='row'>
                         <div className='col-12'>
                             <label for="bio" className="form-label">Bio</label>
-                            <textarea className="form-control" id="bio" rows="2" value={user.data.description}></textarea>
+                            <textarea className="form-control" id="bio" rows="2" value={description} onChange={e=> setDescription(e.target.value)}></textarea>
                         </div>
                     </div>
                 </div>
             </Modal.Body>
             <Modal.Footer>
-            <Button variant="secondary" onClick={() => setShow(false)}>
+            <Button variant="secondary" onClick={handleClose}>
                 Close
             </Button>
-            <Button variant="primary" onClick={() => setShow(false)}>
+            <Button variant="primary" onClick={handleClose}>
                 Save Changes
             </Button>
             </Modal.Footer>
