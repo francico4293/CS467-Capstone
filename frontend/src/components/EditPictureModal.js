73280e50c640fb7ba416fa86da484fc989e8e91c
Colin Francis <francico@oregonstate.edu>
2023-02-01 19:59:58 -0500

    implemented profile picture preview and crop functionality

--- frontend/src/components/EditPictureModal.js
+++ frontend/src/components/EditPictureModal.js
@@ -3,10 +3,13 @@ import { useSelector, useDispatch } from 'react-redux';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { uploadProfilePicture } from '../actions/userActions';
+import Cropper from 'react-easy-crop';
 
 const EditPictureModal = ({ show, setShow, setPictureUploading }) => {
-    const [picture, setPicture] = useState(null);
     const { user, theme } = useSelector(state => state);
+    const [picture, setPicture] = useState(null);
+    const [crop, setCrop] = useState({ x: 0, y: 0 });
+    const [zoom, setZoom] = useState(1);
     const dispatch = useDispatch();
 
     const setError = () => {
@@ -18,20 +21,45 @@ const EditPictureModal = ({ show, setShow, setPictureUploading }) => {
         setShow(false);
     }
 
+    const handleClose = () => {
+        setPicture(null);
+        setShow(false);
+    }
+
     return (
-        <Modal id={`${theme}`} className='edit-picture-modal' show={show} onHide={() => setShow(false)} centered>
-            <Modal.Header closeButton></Modal.Header>
+        <Modal id={`${theme}`} className='edit-picture-modal' show={show} onHide={handleClose} centered>
+            <Modal.Header closeButton>Update Profile Picture</Modal.Header>
             <Modal.Body>
-                <div className='d-flex justify-content-center'>
-                    <img src={user.auth.photoURL ? user.auth.photoURL : `/imgs/profile-image.svg`} className='img-thumbnail shadow-sm mt-3' width={'70%'}/>
-                </div>
+                {
+                    picture
+                        ? (
+                            <>
+                                <div className='crop-container d-flex justify-content-center'>
+                                    <Cropper
+                                        image={picture}
+                                        crop={crop}
+                                        zoom={zoom}
+                                        aspect={1}
+                                        onCropChange={setCrop}
+                                        onZoomChange={setZoom}
+                                    />
+                                </div>
+                                <div className='mt-3'>
+                                    <label for="pictureZoom" className="form-label">Zoom</label>
+                                    <input type="range" className="form-range" min={1} max={3} step={0.1} value={zoom} id="pictureZoom" 
+                                        onChange={e => setZoom(e.target.value)}></input>
+                                </div>
+                            </>
+                        )
+                        : <></>
+                }
                 <div className='mt-3'>
-                    <label for='profile-picture' className='form-label'>Update profile picture</label>
-                    <input type="file" className="form-control" id="profile-picture" onChange={(e) => setPicture(e.target.files[0])}></input>
+                    <label for='profile-picture' className='form-label'>Select image</label>
+                    <input type="file" className="form-control" id="profile-picture" onChange={(e) => setPicture(URL.createObjectURL(e.target.files[0]))}></input>
                 </div>
             </Modal.Body>
             <Modal.Footer>
-            <Button variant="secondary" onClick={() => setShow(false)}>
+            <Button variant="secondary" onClick={handleClose}>
                 Close
             </Button>
             <Button variant="primary" onClick={handleSave}>
