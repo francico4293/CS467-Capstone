d80fa0830ed93de428b624804a39c4c4eb24e983
Colin Francis <francico@oregonstate.edu>
2023-02-09 13:48:23 -0500

    initial layout work for contact cards

--- frontend/src/components/ContactCard.js
+++ frontend/src/components/ContactCard.js
@@ -1,18 +1,33 @@
 import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 
 const ContactCard = ({ name }) => {
     return (
-        <Card style={{ width: '25rem' }}>
-            <Card.Body>
-                <Card.Title>{name}</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-                <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
+        <Card style={{ width: '25rem', minHeight: '12rem' }}>
+            <Card.Body className='mt-3'>
+                <Card.Title>{name}<i className='fa-brands fa-linkedin ms-2'/></Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Google</Card.Subtitle>
+                <Card.Subtitle className="mb-2 text-muted">Sr. Software Engineer</Card.Subtitle>
+                <Card.Text className='mt-3'>
+                    <Row>
+                        <Col className='fw-light'>
+                            <i className='fa-solid fa-envelope me-2'/>fakeemail@gmail.com
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col className='fw-light'>
+                            <i className='fa-solid fa-phone me-2'/>123-456-7890
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col className='d-flex justify-content-end'>
+                            <i className='fa-solid fa-pen me-2'/>
+                            <i className='fa-solid fa-trash'/>
+                        </Col>
+                    </Row>
                 </Card.Text>
-                <Card.Link href="#">Card Link</Card.Link>
-                <Card.Link href="#">Another Link</Card.Link>
             </Card.Body>
         </Card>
     )
