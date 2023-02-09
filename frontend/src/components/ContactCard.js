2db3ebc9da1d28e3e653ceaa96f55dd56ea8d07d
Colin Francis <francico@oregonstate.edu>
2023-02-09 14:16:13 -0500

    added logo layout for contact card

--- frontend/src/components/ContactCard.js
+++ frontend/src/components/ContactCard.js
@@ -2,15 +2,26 @@ import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
 
 const ContactCard = ({ name }) => {
     return (
         <Card style={{ width: '25rem', minHeight: '12rem' }}>
             <div className='contact-color' style={{ backgroundColor: 'aqua' }}></div>
             <Card.Body>
-                <Card.Title>{name}<i className='fa-brands fa-linkedin ms-2'/></Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">Google</Card.Subtitle>
-                <Card.Subtitle className="mb-2 text-muted">Sr. Software Engineer</Card.Subtitle>
+                <Row>
+                    <Col xs={8}>
+                        <Card.Title>{name}<i className='fa-brands fa-linkedin ms-2'/></Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">Google</Card.Subtitle>
+                        <Card.Subtitle className="mb-2 text-muted">Sr. Software Engineer</Card.Subtitle>
+                    </Col>
+                    <Col className='d-flex justify-content-end align-items-start'>
+                        <Image 
+                            src='/img/microsoft-icon.svg'
+                            width={'50%'}
+                        />
+                    </Col>
+                </Row>
                 <Card.Text className='mt-3'>
                     <Row>
                         <Col className='fw-light'>
