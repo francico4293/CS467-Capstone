4800c9d84878738a4aad49ecbd6ee2f8a42e98d4
Colin Francis <francico@oregonstate.edu>
2023-02-17 02:25:37 -0500

    very generic initial ui layout for job board page

--- frontend/src/pages/JobBoard.js
+++ frontend/src/pages/JobBoard.js
@@ -3,6 +3,7 @@ import Sidebar from '../components/Sidebar';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Filter from '../components/Filter';
 
 const JobBoard = () => {
     return (
@@ -10,7 +11,19 @@ const JobBoard = () => {
             <Row>
                 <Sidebar/>
                 <Col xs={10} sm={9} md={10} className='ms-auto'>
-                    
+                    <Row className='mt-3 mb-3'>
+                        <Col className='d-flex border-bottom pb-3'>
+                            <Filter items={[]} setItems={null}/>
+                            <Filter items={[]} setItems={null}/>
+                            <Filter items={[]} setItems={null}/>
+                        </Col>
+                    </Row>
+                    <Row className='d-flex flex-nowrap'>
+                        <Col xs={3} className='test me-3 ms-3'></Col>
+                        <Col xs={3} className='test me-3'></Col>
+                        <Col xs={3} className='test me-3'></Col>
+                        <Col xs={3} className='test me-3'></Col>
+                    </Row>
                 </Col>
             </Row>
         </Container>
