a3b696e2db85df63de404277e1101887bc1b03e9
Ron Riza <ronriza91@gmail.com>
2023-02-27 00:02:51 -0500

    wrote database operations for creating and getting jobs

---
+++ backend/models/jobs.js
@@ -0,0 +1,31 @@
+const { db } = require('../fire-admin');
+
+createJob = async (uid, columnName, jobData) => {
+    const jobRef = await db.collection('jobs').add(jobData);
+
+    const userRef = db.collection('users').doc(uid);
+    const user = await userRef.get()
+    const userData = user.data()
+    userData.columns.forEach(column => {
+        if (column.name === columnName) {
+            column.jobs.push(jobRef.id)
+        }
+    });
+    await userRef.update(userData)
+}
+
+getJobs = async (uid) => {
+    const userRef = db.collection('users').doc(uid);
+    const user = await userRef.get()
+    const columns = user.data().columns
+
+    const populatedColumns = await Promise.all(columns.map(async column => {
+        const jobs = await Promise.all(column.jobs.map(async jobId => {
+            const jobRef = db.collection('jobs').doc(jobId)
+            const job = await jobRef.get()
+            return {...job.data(), id: jobId}
+        }))
+        return {name: column.name, jobs }
+    }))
+    return populatedColumns
+}
