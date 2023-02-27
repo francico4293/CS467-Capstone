1b849a4976849813201e4114078c82dc1d330247
Ron Riza <ronriza91@gmail.com>
2023-02-27 00:04:17 -0500

    modified way in which jobs are counted in user profile

--- frontend/src/components/UserProfile.js
+++ frontend/src/components/UserProfile.js
@@ -29,7 +29,7 @@ const UserProfile = ({ setShowEditProfileModal }) => {
             </div>
             <div className='overview'>
                 <p className='fw-light mt-3'>Profile Overview:</p>
-                <p className='fw-light'><i className='fa-solid fa-chart-line me-1'/>{`Tracking ${user.data.jobs.length} jobs`}</p>
+                <p className='fw-light'><i className='fa-solid fa-chart-line me-1'/>{`Tracking ${user.data.columns.reduce((acc, column) => acc + column.jobs.length, 0)} jobs`}</p>
                 <p className='fw-light'><i className='fa-solid fa-circle-nodes me-1'/>{`Managing ${user.data.contacts.length} contacts`}</p>
             </div>
         </>
