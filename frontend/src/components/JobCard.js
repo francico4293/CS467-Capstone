0c5200c3126beb827432358f4479de160f2ad359
Colin Francis <francico@oregonstate.edu>
2023-02-28 16:21:52 -0500

    added job skill badges to job cards

--- frontend/src/components/JobCard.js
+++ frontend/src/components/JobCard.js
@@ -4,6 +4,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 import ReactTimeAgo from 'react-time-ago';
+import Badge from 'react-bootstrap/Badge';
 
 const JobCard = ({ job }) => {
     return (
@@ -18,7 +19,9 @@ const JobCard = ({ job }) => {
                         fluid
                     />
                     <Row>
-                        <Col className='fw-bold fs-5'>{job.company}<i className='fa-solid fa-link ms-1'/></Col>
+                        <Col className='fw-bold fs-5'>
+                            {job.company}{job.link ? <i className='fa-solid fa-link ms-1'/> : <></>}
+                        </Col>
                     </Row>
                     <Row>
                         <Col className='text-muted'>{job.jobTitle}</Col>
@@ -26,7 +29,12 @@ const JobCard = ({ job }) => {
                     <Row>
                         <Col className='text-muted'>{`${job.city}, ${job.state}`}</Col>
                     </Row>
-                    <Row className='mt-2'>
+                    <Row>
+                        <Col className='overflow-auto d-flex flex-nowrap pt-2 pb-2'>
+                            {job.skills.map((skill, idx) => <Badge className='me-1' key={idx}>{skill}</Badge>)}
+                        </Col>
+                    </Row>
+                    <Row>
                         <Col className='d-flex justify-content-between text-muted'>
                             <div>
                                 Job created <ReactTimeAgo date={job.created} locale='en-US'/>
